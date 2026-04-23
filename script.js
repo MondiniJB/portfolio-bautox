bb093635eb6d72b44535e22c411d309fc3f8e1cd
MondiniJB <84585078+MondiniJB@users.noreply.github.com>
2026-04-23 00:29:53 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,14 @@
+interact('.draggable').draggable({
+    listeners: {
+        move (event) {
+            var target = event.target;
+            var x = (parseFloat(target.getAttribute('data-x')) || 0) + event.dx;
+            var y = (parseFloat(target.getAttribute('data-y')) || 0) + event.dy;
+
+            target.style.transform = `translate(${x}px, ${y}px)`;
+
+            target.setAttribute('data-x', x);
+            target.setAttribute('data-y', y);
+        }
+    }
+});
